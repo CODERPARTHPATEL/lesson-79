01521d2edfdaf3a6ce3969b6aaad4e84ea422d9a
CODERPARTHPATEL <nskp.patel09@gmail.com>
2026-05-29 19:35:05 +0100

    lesson 79

--- line2.py
+++ line2.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.array([1,2,3,4])
-
+y = x*2
 plt.plot(x,y)
 
 x1 = [2,4,6,8]
@@ -13,5 +13,5 @@ plt.xlabel('x-axis data')
 plt.ylabel('y-axis data')
 plt.title('multiple plots')
 
-plt.fill_between(x,y,y1,color='green';alpha=0.5)
+plt.fill_between(x,y,y1,color='green',alpha=0.5)
 plt.show()
